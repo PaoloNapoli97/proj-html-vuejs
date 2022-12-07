f5bf13d009d54a793da7bac6b38db280f449acd3
PaoloNapoli97 <paolonapoli@live.it>
2022-12-07 02:47:15 +0100

    Add: LastestNews&NewsCard + button Load More Js

--- src/components/LatestNews.vue
+++ src/components/LatestNews.vue
@@ -8,11 +8,11 @@
           <p>
             Every week we publish content about what is the best business world.
           </p>
-          <button class="btn btn-primary">SEE ALL</button>
+          <button class="btn btn-primary" @click="loadMore">SEE ALL</button>
         </div>
       </div>
       <div class="cards-container">
-        <NewsCard class="card" />
+        <NewsCard v-for="card in cardsLoaded" class="card" :info="card" />
       </div>
     </div>
   </main>
@@ -26,7 +26,42 @@ export default {
     NewsCard,
   },
   data() {
-    return {};
+    return {
+      cards: [
+        {
+          image: "news-1.jpg",
+          text: "Increasing creativity is possible for everyone",
+        },
+        {
+          image: "news-2.jpg",
+          text: "Because market research is part of the business plan",
+        },
+        {
+          image: "news-3.jpg",
+          text: "Working from home is now a trend",
+        },
+        {
+          image: "news-4.jpg",
+          text: "Se ni’ mondo esistesse un po’ di bene e ognun si honsiderasse suo fratello",
+        },
+        {
+          image: "news-5.jpg",
+          text: "ci sarebbe meno pensieri e meno pene e il mondo ne sarebbe assai più bello",
+        },
+      ],
+      length: 3,
+    };
+  },
+  methods: {
+    loadMore() {
+      if (this.length > this.cards.length) return;
+      this.length = this.length + 3;
+    },
+  },
+  computed: {
+    cardsLoaded() {
+      return this.cards.slice(0, this.length);
+    },
   },
 };
 </script>
@@ -45,6 +80,14 @@ main {
   }
 
   .cards-container {
+    display: flex;
+    flex-wrap: wrap;
+    // justify-content: space-between;
+    .card {
+      width: calc(100% / 3 - 20px);
+      padding-top: 10px;
+      margin: auto;
+    }
   }
 }
 </style>
--- src/components/NewsCard.vue
+++ src/components/NewsCard.vue
@@ -1,13 +1,36 @@
 <template>
-  <div></div>
+  <div class="card">
+    <img :src="`src/assets/img/${info.image}`" :alt="info.image" />
+    <div class="text">{{ info.text }}</div>
+  </div>
 </template>
 
 <script>
 export default {
-  setup() {
-    return {};
+  props: {
+    info: Object,
   },
 };
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.card {
+  position: relative;
+
+  img {
+    width: 100%;
+    border-radius: 14px;
+    filter: brightness(60%);
+  }
+
+  .text {
+    padding: 0 10px;
+    font-size: 20px;
+    font-weight: 800;
+    color: var(--quaternary-color);
+    position: absolute;
+    bottom: 60px;
+    text-align: center;
+  }
+}
+</style>
